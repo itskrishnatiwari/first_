39646ccd2caf42ad051113d84428b9364e04ebb0
itskrishnatiwari <pankajtiwari45645@gmail.com>
2025-05-05 22:28:43 +0530

    url changed

--- backend/index.js
+++ backend/index.js
@@ -19,7 +19,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cookieParser());
 const corsOption = {
-  origin: "http://localhost:3000",
+  origin: "https://first-1xnn.onrender.com",
   credentials: true,
 };
 app.use(cors(corsOption));
